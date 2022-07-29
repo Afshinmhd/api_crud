7396a731523e4ac4feaa030ce1f32f47700245fb
afshin <mohammadifar.afshin@gmail.com>
2022-07-29 11:42:00 +0430

    add a validator in serializer for user

--- user/serializers.py
+++ user/serializers.py
@@ -1,20 +1,30 @@
+import email
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    #password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
         fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'validators': (validate_password,), 'write_only': True}
+        }
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({'error': 'password fields did not match.'})
         return attrs
+
+    def validate_email(self, value):
+        user = User.objects.filter(email=value)
+        if user:
+            raise serializers.ValidationError({'error':'This email already exist'})
+        return value
 
     def create(self, validated_data):
         del validated_data['password2']
