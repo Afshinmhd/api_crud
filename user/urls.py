0c578d8a9fe672096f869adfb12e6ce26652581e
afshin <mohammadifar.afshin@gmail.com>
2022-07-29 11:10:13 +0430

    add urls.py and change views.py for user

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from atexit import register
+from rest_framework.routers import DefaultRouter
+from .views import RegisterViewSet
+
+
+router = DefaultRouter()
+router.register(r'register', RegisterViewSet, basename='register')
+urlpatterns = router.urls
--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    """
+        This class used to register a user
+    """
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny,]
